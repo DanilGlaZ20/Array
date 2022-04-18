dd711f3fbf5649192ac8f983a46a389ceee94e66
Данила Глазунов <glazdan17@gmail.com>
2022-04-18 23:59:59 +0300

    raw

--- Array/Program.cs
+++ Array/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Array
@@ -21,6 +22,23 @@ namespace Array
             Console.WriteLine();
             Console.WriteLine($"Кол-во пассажиров в поезде:{sum}");
         }
+
+        public static void Common_el()
+        {
+            int number = 4;
+            int[] array = new int[number];
+            for (int i = 0; i < number; i++) array[i] = Convert.ToInt16(Console.ReadLine());
+            for (int i = 0; i < number; i++)
+            {
+                for (int j = 0; j < number; j++)
+                {
+                    if(array[i]==array[j]) Console.WriteLine();
+                }
+                
+                  
+                  
+            }
+            }
 
         static void Main(string[] args)
         {
