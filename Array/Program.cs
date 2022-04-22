b11aae15a14dcc4e629fe6f7d23eac356c2d1256
Данила Глазунов <glazdan17@gmail.com>
2022-04-22 19:35:54 +0300

    Rotation

--- Array/Program.cs
+++ Array/Program.cs
@@ -7,24 +7,27 @@ namespace Array
 {
     class Program
     {
-        public  static void Train()
-        {   Console.Write("Введите кол-во вагонов:");
-            int num = Convert.ToInt16(Console.ReadLine());
-            int sum = 0;
+        public static void
+            Train() //программа получает количество вагонов и число пассажиров; выводит общее кол-во пассажиров
+        {
+            Console.Write("Введите кол-во вагонов:");
+            int num = Convert.ToInt16(Console.ReadLine()); //кол-во вагонов
+            int sum = 0; //кол-во пассажиров в поезде
             int[] array = new int[num];
             Console.WriteLine("Введите кол-во пассажиров в каждом вагоне ");
-            for (int i = 0; i < num; i++)
+            for (int i = 0; i < num; i++) //заполнение массива
             {
                 array[i] = Convert.ToInt16(Console.ReadLine());
                 sum += array[i];
             }
+
             Console.Write("Кол-во пассажиров в вагонах:");
-            for (int j = 0; j < num; j++) Console.Write(array[j]+" ");
+            for (int j = 0; j < num; j++) Console.Write(array[j] + " ");
             Console.WriteLine();
             Console.WriteLine($"Кол-во пассажиров в поезде:{sum}");
         }
 
-        public static void Common_el()
+        public static void Common_el() //нахождение общих элементов в 2 массивах
         {
             int number = 4;
             String[] array1 = new String[number];
@@ -37,19 +40,75 @@ namespace Array
             {
                 for (int j = 0; j < number; j++)
                 {
-                    if (array1[i].Equals(array2[j]))
-                        Console.WriteLine($"Этот элемент присутствует в обоих массивах {array1[i]}");
+                    if (array1[i].Equals(array2[j])) //сравнение 2 массивов
+                        Console.WriteLine($"Этот элемент присутствует в обоих массивах - {array1[i]}");
                 }
             }
 
         }
 
-        static void Main(string[] args)
+        public static void Rotation() //программа получает массив и кол-во поворотов (поворачивает массив)
         {
-            //Train();
-            Common_el();
+            int num = 5;
+            int[] arr = new int[num];
+            for (int i = 0; i < num; i++) arr[i] = Convert.ToInt16(Console.ReadLine()); //ввод массива
+            Console.Write("Введите кол-во поворотов: ");
+            int p = Convert.ToInt16(Console.ReadLine()); //кол-во поворотов
+
+            for (int i = 0; i < p; i++)
+            {
+                int j;
+                int tmp = arr[0];
+                for (j = 0; j < num - 1; j++)
+                {
+                    arr[j] = arr[j + 1];
+
+                }
+
+                arr[j] = tmp;
+            }
+
+
+
+            Console.Write("Перевернутый массив:");
+            for (int k = 0; k < num; k++) Console.Write(arr[k] + " ");
+
         }
+
+        public static void Big_number()
+        {
+            Console.Write("Введите кол-во элементов в массиве: ");
+            int num = Convert.ToInt16(Console.ReadLine());
+            int[] arr = new int[num];
+            Console.WriteLine("Введите  элементы  массива: ");
+            for (int i = 0; i < arr.Length; i++) arr[i] = Convert.ToInt16(Console.ReadLine());
+            int tmp=0;
+            Console.Write("Отсортированный массив: ");
+            for (int j = 0; j <= arr.Length - 1; j++)
+            {
+                for (int i = 0; i < arr.Length-1; i++)
+                    {
+                        if (arr[j] < arr[i])
+                        {
+                            Console.Write(arr[j] + " ");
+                        }
+                        break;
+                    }
+                //if( arr[j]==arr[arr.Length-1])Console.Write(arr[j] );
+            }
+
+        }
+
+        static void Main(string[] args)
+            {
+                //Train();
+                //Common_el();
+                //Rotation();
+                Big_number();
+            }
+
         
+
     }
 }
 
